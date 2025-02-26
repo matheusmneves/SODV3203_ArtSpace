1e0e09100784bfc5360c5a61ffe00686d50652f6
matheusmneves <matheusneves.m@gmail.com>
2025-02-26 14:09:07 -0700

    First commit

---
+++ app/src/main/java/com/example/artspaceapp/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.example.artspaceapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val GreenLight = Color(0xFF4CAF50)
+val GreenDark = Color(0xFF388E3C)
+val GreenDarker = Color(0xFF2E7D32)
